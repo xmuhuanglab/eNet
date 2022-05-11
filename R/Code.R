c5e0739f57eb4aa33c6fceba5d156d7430e7564d
xmuhuanglab <95668602+xmuhuanglab@users.noreply.github.com>
2022-05-11 20:15:05 +0800

    Update Code.R

--- R/Code.R
+++ R/Code.R
@@ -26,6 +26,7 @@ source('./utils.R')
 ###################################################################
 #-----Step 2. Identifying the putative enhancer cluster (Node)-----
 ###################################################################
+# --- 2.1 calculate gene-peak correlation
 GPTab <- GPCor(cre.mat=cre.mat,  # peak-cell matrix
                exp.mat=rna.mat,  # scRNA-seq matrix or gene activity matrix
                normalizeRNAMat=T, # if the rna matrix need normalization
@@ -35,6 +36,7 @@ GPTab <- GPCor(cre.mat=cre.mat,  # peak-cell matrix
                nCores=8 # How many registerCores to use
 )
 save(GPTab, file = 'genePeakTab.Rdata')
+# --- 2.2 obtain significantly correlated peak-gene pairs
 # For determining the threshold value of estimate, namely peak-gene correlation, we advice to choose the value of quantile 95% of overall estimate using quantile(GPTab$estimate, seq(0,1,0.05))[['95%']]
 GPTabFilt <- FindNode(GPTab = GPTab, # data frame of gene-peak correlation
                       genome = "hg19", # reference genome, must be one of "hg19", "mm10", or "hg38"
@@ -43,7 +45,7 @@ GPTabFilt <- FindNode(GPTab = GPTab, # data frame of gene-peak correlation
                       FDR = 0.05 # the threshold value of FDR to determine whether the peak-gene pair is significantly correlated
 )
 save(GPTabFilt, file = 'genePeakTabFilt.Rdata')
-# enhancer cluster list
+# --- 2.3 convert the table of gene-peak pairs to a list of enhancer cluster
 GPPair <- list()
 for(i in unique(GPTabFilt$Gene)){
   tmp <- subset(GPTabFilt, Gene == i)
@@ -73,6 +75,7 @@ save(conns, file = 'conns_GPPairPeaks.Rdata')
 library(igraph)
 library(dplyr)
 library(doParallel)
+# To determine the threshold value of co-accssibility score, we advice to choose the value around quantile 90%-95% using quantile(subset(conns, coaccess>0)$coaccess,seq(0,1,0.05),na.rm = T)
 NetworkList <- BuildNetwork(conns=conns, # A data frame of co-accessibility scores, also the output of Step.3
                             GPTab=GPTabFilt,  # a list of enhancer cluster, the output of Step.2
                             cutoff=0.1, # the cutoff of co-accessibility score to determine whether the enhancer pairs are significantly co-accessible.
@@ -87,6 +90,7 @@ plot.igraph(NetworkList[["ESPN"]])
 ###################################################################
 #-----Step 5.Calculating network complexity(Network complexity)----
 ###################################################################
+# To determine the threshold value of co-accssibility score, we advice to choose the value around quantile 90%-95% using quantile(subset(conns, coaccess>0)$coaccess,seq(0,1,0.05),na.rm = T)
 Networkinfo <- NetComplexity(conns=conns,  # enhancer-enhancer co-accessibilty calculated using Cicero
                              GPTab=GPTabFilt,   # a list of enhancer cluster, also the output of Step.2
                              cutoff=0.1,  # the threshold value of co-accessibility score to determine whether the enhancer pairs are significantly co-accessible.
@@ -99,6 +103,8 @@ save(Networkinfo, file = "Networkinfo.Rdata")
 ###################################################################
 #--------Step 6. Classification of enhancer networks (Mode)--------
 ###################################################################
+# To determine the threshold value of network size, namely SizeCutoff in the function NetworkMode, we advice to choose the median of network size for all genes.
+# To determine the threshold value of network connectivity, namely ConnectivityCutoff in the function NetworkMode, we advice to use value 1 to ensure that on average each enhancer has at least one interaction with other enhancer.
 Mode <- NetworkMode(Networkinfo=Networkinfo,  # Network information, the output file in Step.5
                     SizeCutoff=5, # this parameter represents the threshold value of network size, which can be used to distinguish Simple and Network/Multiple mode
                     ConnectivityCutoff=1 # this parameter represents the threshold value of network connectivity, which can be used to distinguish Network and Multiple mode
